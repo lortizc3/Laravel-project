d8b4fa77fc8428a6a7ae87aa49cd25c4a21ce93e
lortizc3 <89048449+lortizc3@users.noreply.github.com>
2024-03-05 15:59:53 -0500

    Add files via upload

---
+++ storage/framework/views/112a7754bd0ef74624b3b40d418fe2f1.php
@@ -0,0 +1,46 @@
+<?php $__env->startSection("title", "Image Storage - DI"); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<div class="container">
+
+<div class="row justify-content-center">
+
+<div class="col-md-8">
+
+<div class="card">
+
+<div class="card-header">Upload image</div>
+
+<div class="card-body">
+
+<form action="<?php echo e(route('imagenotdi.save')); ?>" method="post" enctype="multipart/form-data">
+
+<?php echo csrf_field(); ?>
+
+<div class="form-group">
+
+<label>Image:</label>
+
+<input type="file" name="profile_image" />
+
+</div>
+
+<button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+
+<img src="<?php echo e(URL::asset('storage/test.png')); ?>" />
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/laravelcourse/resources/views/imagenotdi/index.blade.php ENDPATH**/ ?>
